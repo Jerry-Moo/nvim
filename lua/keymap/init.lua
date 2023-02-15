30043039a5e3c8bb4f94f07e71671071d98c2948
JerryMoo <wuchujie123@gmail.com>
2023-02-16 00:53:00 +0800

    add: add keymap

---
+++ lua/keymap/init.lua
@@ -0,0 +1,36 @@
+local api = vim.api
+-- require('keymap.remap')
+local keymap = require('core.keymap')
+local nmap, imap, xmap = keymap.nmap, keymap.imap, keymap.xmap
+local silent, noremap = keymap.silent, keymap.noremap
+local expr, remap = keymap.expr, keymap.remap
+local opts = keymap.new_opts
+local cmd = keymap.cmd
+-- require('keymap.config')
+
+-- Use space as leader key
+vim.g.mapleader = ' '
+
+-- leaderkey
+nmap({ ' ', '', opts(noremap) })
+xmap({ ' ', '', opts(noremap) })
+
+imap({
+  -- tab key
+  { '<TAB>', _G.smart_tab, opts(expr, remap) },
+  { '<S-TAB>', _G.smart_shift_tab, opts(expr, remap) },
+})
+
+-- usage example
+nmap({
+  -- plugin manager: Lazy.nvim
+  { '<Leader>pu', cmd('Lazy update'), opts(noremap, silent) },
+  { '<Leader>pi', cmd('Lazy install'), opts(noremap, silent) },
+  -- nvimtree
+  { '<Leader>e', cmd('NvimTreeToggle'), opts(noremap, silent) },
+  -- Telescope
+  { '<Leader>fb', cmd('Telescope buffers'), opts(noremap, silent) },
+  { '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
+  { '<Leader>fw', cmd('Telescope grep_string'), opts(noremap, silent) },
+  { '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
+})
---
+++ lua/keymap/remap.lua
@@ -0,0 +1,64 @@
+local keymap = require('core.keymap')
+local nmap, imap, cmap, tmap = keymap.nmap, keymap.imap, keymap.cmap, keymap.tmap
+local silent, noremap = keymap.silent, keymap.noremap
+local expr = keymap.expr
+local opts = keymap.new_opts
+local cmd = keymap.cmd
+
+-- noremal remap
+nmap({
+  -- close buffer
+  { '<C-x>k', cmd('bdelete'), opts(noremap, silent) },
+  -- save
+  { '<C-s>', cmd('write'), opts(noremap) },
+  -- yank
+  { 'Y', 'y$', opts(noremap) },
+  -- buffer jump
+  { ']b', cmd('bn'), opts(noremap) },
+  { '[b', cmd('bp'), opts(noremap) },
+  -- force quit all
+  { '<C-q>', cmd('qa!') },
+  -- remove trailing white space
+  { '<Leader>t', cmd('TrimTrailingWhitespace'), opts(noremap) },
+  -- window jump
+  { '<C-h>', '<C-w>h', opts(noremap) },
+  { '<C-l>', '<C-w>l', opts(noremap) },
+  { '<C-j>', '<C-w>j', opts(noremap) },
+  { '<C-k>', '<C-w>k', opts(noremap) },
+  -- resize window
+  { '<A-[>', cmd('vertical resize -5') },
+  { '<A-]>', cmd('vertical resize +5') },
+})
+
+-- insertmode remap
+imap({
+  { '<C-w>', '<C-[>diwa', opts(noremap) },
+  { '<C-h>', '<Bs>', opts(noremap) },
+  { '<C-d>', '<Del>', opts(noremap) },
+  { '<C-u>', '<C-G>u<C-u>', opts(noremap) },
+  { '<C-b>', '<Left>', opts(noremap) },
+  { '<C-f>', '<Right>', opts(noremap) },
+  { '<C-a>', '<Esc>^i', opts(noremap) },
+  { '<C-j>', '<Esc>o', opts(noremap) },
+  { '<C-k>', '<Esc>O', opts(noremap) },
+  { '<C-s>', '<ESC>:w<CR>', opts(noremap) },
+  {
+    '<C-e>',
+    function()
+      return vim.fn.pumvisible() == 1 and '<C-e>' or '<End>'
+    end,
+    opts(noremap, expr),
+  },
+})
+
+-- commandline remap
+cmap({
+  { '<C-b>', '<Left>' , opts(noremap, silent) },
+  { '<C-f>', '<Right>' , opts(noremap, silent) },
+  { '<C-a>', '<Home>' , opts(noremap, silent) },
+  { '<C-e>', '<End>' , opts(noremap, silent) },
+  { '<C-d>', '<Del>' , opts(noremap, silent) },
+  { '<C-h>', '<BS>' , opts(noremap, silent) },
+})
+
+tmap({ '<Esc>', [[<C-\><C-n>]] })
