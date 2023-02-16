3ed8d0fbc3c67dc013eb67c59c66ab9f085b94f4
JerryMoo <wuchujie123@gmail.com>
2023-02-17 01:37:56 +0800

    :add base tools

---
+++ lua/modules/tools/package.lua
@@ -0,0 +1,12 @@
+local package = require('core.pack').package
+local conf = require('modules.tools.config')
+
+package({
+  'nvim-telescope/telescope.nvim',
+  cmd = 'Telescope',
+  config = conf.telescope,
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope-fzy-native.nvim' },
+  },
+})
