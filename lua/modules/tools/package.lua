dd26c8d545a1a12a83db96c1ae3d10a85a6b6ab1
JerryMoo <wuchujie123@gmail.com>
2023-03-01 01:43:57 +0800

    add tools module css colorizer

--- lua/modules/tools/package.lua
+++ lua/modules/tools/package.lua
@@ -1,6 +1,14 @@
 local package = require('core.pack').package
 local conf = require('modules.tools.config')
 
+package({ -- css 着色器
+  'norcalli/nvim-colorizer.lua',
+  ft = { 'lua', 'css', 'html', 'sass', 'less', 'typescriptreact' },
+  config = function()
+    require('colorizer').setup()
+  end,
+})
+
 package({
   'nvim-telescope/telescope.nvim',
   cmd = 'Telescope',
