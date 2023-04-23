3773dbb7a29e750e9c6a8dcbf6d167363f3ffaa4
JerryMoo <wuchujie123@gmail.com>
2023-04-24 01:40:22 +0800

    :lua/telescopt/app.lua

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -1,40 +1,40 @@
 local api = vim.api
-require('keymap.remap')
-local map = require('core.keymap')
+require("keymap.remap")
+local map = require("core.keymap")
 local cmd = map.cmd
 
 -- usage example
 map.n({
-  -- plugin manager: Lazy.nvim
-  ['<Leader>pu'] = cmd('Lazy update'),
-  ['<Leader>pi'] = cmd('Lazy install'),
-  -- nvimtree
-  ['<Leader>e'] = cmd('NvimTreeToggle'),
-  -- Telescope
-  ['<Leader>fb'] = cmd('Telescope buffers'),
-  ['<Leader>fa'] = cmd('Telescope live_grep'),
-  ['<Leader>fw'] = cmd('Telescope grep_string'),
-  ['<Leader>ff'] = cmd('Telescope find_files find_command=rg,--ignore,--hidden,--files'),
-  -- Lsp
-  ['<Leader>li'] = cmd('LspInfo'),
-  ['<Leader>ll'] = cmd('LspLog'),
-  ['<Leader>lr'] = cmd('LspRestart'),
-  -- Lspsaga
-  ['K'] = cmd('Lspsaga hover_doc'),
-  ['gd'] = cmd('Lspsaga peek_definition'),
-  ['gD'] = cmd('lua vim.lsp.buf.definition()'), -- 比 cmd('Lspsaga goto_definition') 好用
+	-- plugin manager: Lazy.nvim
+	["<Leader>pu"] = cmd("Lazy update"),
+	["<Leader>pi"] = cmd("Lazy install"),
+	-- nvimtree
+	["<Leader>e"] = cmd("NvimTreeToggle"),
+	-- Telescope
+	["<Leader>fb"] = cmd("Telescope buffers"),
+	["<Leader>fa"] = cmd("Telescope live_grep"),
+	["<Leader>fw"] = cmd("Telescope grep_string"),
+	["<Leader>ff"] = cmd("Telescope find_files find_command=rg,--ignore,--hidden,--files"),
+	-- Lsp
+	["<Leader>li"] = cmd("LspInfo"),
+	["<Leader>ll"] = cmd("LspLog"),
+	["<Leader>lr"] = cmd("LspRestart"),
+	-- Lspsaga
+	["K"] = cmd("Lspsaga hover_doc"),
+	["gd"] = cmd("Lspsaga peek_definition"),
+	["gD"] = cmd("lua vim.lsp.buf.definition()"), -- 比 cmd('Lspsaga goto_definition') 好用
 })
 
-map.n('<Leader>E', function()
-  vim.cmd('Telescope file_browser')
-  local esc_key = api.nvim_replace_termcodes('<Esc>', true, false, true)
-  api.nvim_feedkeys(esc_key, 'n', false)
+map.n("<Leader>E", function()
+	vim.cmd("Telescope file_browser")
+	local esc_key = api.nvim_replace_termcodes("<Esc>", true, false, true)
+	api.nvim_feedkeys(esc_key, "n", false)
 end)
 
-map.n('-', function()
-  local ok, pick_window = pcall(require, 'window-picker')
-  if ok then
-    local picked_window_id = pick_window.pick_window() or vim.api.nvim_get_current_win()
-    vim.api.nvim_set_current_win(picked_window_id)
-  end
+map.n("-", function()
+	local ok, pick_window = pcall(require, "window-picker")
+	if ok then
+		local picked_window_id = pick_window.pick_window() or vim.api.nvim_get_current_win()
+		vim.api.nvim_set_current_win(picked_window_id)
+	end
 end)
--- lua/keymap/remap.lua
+++ lua/keymap/remap.lua
@@ -1,74 +1,76 @@
-local map = require('core.keymap')
+local map = require("core.keymap")
 local cmd = map.cmd
 
 map.n({
-  -- close buffer
-  ['<C-x>k'] = cmd('bdelete'),
-  -- save
-  ['<C-s>'] = cmd('write'),
-  -- buffer jump
-  [']b'] = cmd('bn'),
-  ['[b'] = cmd('bp'),
-  -- force quit all
-  ['<C-q>'] = cmd('qa!'),
-  -- remove trailing white space
-  ['<Leader>t'] = cmd('TrimTrailingWhitespace'),
-  -- yank
-  ['Y'] = 'y$',
-  -- switch window
-  ['<C-h>'] = '<C-w>h',
-  ['<C-l>'] = '<C-w>l',
-  ['<C-j>'] = '<C-w>j',
-  ['<C-k>'] = '<C-w>k',
-  -- resize window
-  ['<A-h>'] = cmd('vertical resize -5'),
-  ['<A-l>'] = cmd('vertical resize +5'),
-  ['<A-j>'] = cmd('resize +5'),
-  ['<A-k>'] = cmd('resize -5'),
-  -- window
-  ['<Leader>1'] = '1gt',
-  ['<Leader>2'] = '2gt',
-  ['<Leader>3'] = '3gt',
-  ['<Leader>4'] = '4gt',
-  ['<Leader>5'] = '5gt',
-  ['<Leader>6'] = '6gt',
-  ['<Leader>7'] = '7gt',
-  ['<Leader>8'] = '8gt',
-  ['<Leader>9'] = '9gt',
-  ['<Leader>0'] = cmd('tablast'),
-  -- Mac
-  ['˙'] = cmd('vertical resize -5'),
-  ['¬'] = cmd('vertical resize +5'),
-  ['∆'] = cmd('resize +5'),
-  ['˚'] = cmd('resize -5'),
+	["j"] = "gj",
+	["k"] = "gk",
+	-- close buffer
+	["<C-x>k"] = cmd("bdelete"),
+	-- save
+	["<C-s>"] = cmd("write"),
+	-- buffer jump
+	["]b"] = cmd("bn"),
+	["[b"] = cmd("bp"),
+	-- force quit all
+	["<C-q>"] = cmd("qa!"),
+	-- remove trailing white space
+	["<Leader>t"] = cmd("TrimTrailingWhitespace"),
+	-- yank
+	["Y"] = "y$",
+	-- switch window
+	["<C-h>"] = "<C-w>h",
+	["<C-l>"] = "<C-w>l",
+	["<C-j>"] = "<C-w>j",
+	["<C-k>"] = "<C-w>k",
+	-- resize window
+	["<A-h>"] = cmd("vertical resize -5"),
+	["<A-l>"] = cmd("vertical resize +5"),
+	["<A-j>"] = cmd("resize +5"),
+	["<A-k>"] = cmd("resize -5"),
+	-- window
+	["<Leader>1"] = "1gt",
+	["<Leader>2"] = "2gt",
+	["<Leader>3"] = "3gt",
+	["<Leader>4"] = "4gt",
+	["<Leader>5"] = "5gt",
+	["<Leader>6"] = "6gt",
+	["<Leader>7"] = "7gt",
+	["<Leader>8"] = "8gt",
+	["<Leader>9"] = "9gt",
+	["<Leader>0"] = cmd("tablast"),
+	-- Mac
+	["˙"] = cmd("vertical resize -5"),
+	["¬"] = cmd("vertical resize +5"),
+	["∆"] = cmd("resize +5"),
+	["˚"] = cmd("resize -5"),
 })
 
 -- insertmode remap
 map.i({
-  ['<C-w>'] = '<C-[>diwa',
-  ['<C-h>'] = '<Bs>',
-  ['<C-d>'] = '<Del>',
-  ['<C-u>'] = '<C-G>u<C-u>',
-  ['<C-b>'] = '<Left>',
-  ['<C-f>'] = '<Right>',
-  ['<C-a>'] = '<Esc>^i',
-  ['<C-j>'] = '<Esc>o',
-  ['<C-k>'] = '<Esc>O',
-  ['<C-s>'] = '<ESC>:w<CR>',
+	["<C-w>"] = "<C-[>diwa",
+	["<C-h>"] = "<Bs>",
+	["<C-d>"] = "<Del>",
+	["<C-u>"] = "<C-G>u<C-u>",
+	["<C-b>"] = "<Left>",
+	["<C-f>"] = "<Right>",
+	["<C-a>"] = "<Esc>^i",
+	["<C-j>"] = "<Esc>o",
+	["<C-k>"] = "<Esc>O",
+	["<C-s>"] = "<ESC>:w<CR>",
 })
 
-map.i('<c-e>', function()
-  return vim.fn.pumvisible() == 1 and '<C-e>' or '<End>'
+map.i("<c-e>", function()
+	return vim.fn.pumvisible() == 1 and "<C-e>" or "<End>"
 end, { expr = true })
 
 -- commandline remap
 map.c({
-  ['<C-b>'] = '<Left>',
-  ['<C-f>'] = '<Right>',
-  ['<C-a>'] = '<Home>',
-  ['<C-e>'] = '<End>',
-  ['<C-d>'] = '<Del>',
-  ['<C-h>'] = '<BS>',
+	["<C-b>"] = "<Left>",
+	["<C-f>"] = "<Right>",
+	["<C-a>"] = "<Home>",
+	["<C-e>"] = "<End>",
+	["<C-d>"] = "<Del>",
+	["<C-h>"] = "<BS>",
 })
 
-map.t('<Esc>', [[<C-\><C-n>]])
+map.t("<Esc>", [[<C-\><C-n>]])
