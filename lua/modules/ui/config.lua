bbabb8492c29f4f2032cf759816af6221ecfd4a7
JerryMoo <wuchujie123@gmail.com>
2023-04-24 20:59:59 +0800

    update: update ui modules

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -1,150 +1,150 @@
 local config = {}
 
 function config.whisky()
-  require('whiskyline').setup()
+	require("whiskyline").setup()
 end
 
 function config.dashboard()
-  local db = require('dashboard')
-  db.setup({
-    theme = 'hyper',
-    config = {
-      week_header = {
-        enable = true,
-      },
-      project = {
-        enable = true,
-      },
-      disable_move = true,
-      shortcut = {
-        {
-          desc = 'Update',
-          icon = ' ',
-          group = 'Include',
-          action = 'Lazy update',
-          key = 'u'
-        },
-        {
-          icon = ' ',
-          desc = 'Files',
-          group = 'Function',
-          action = 'Telescope find_files find_command=rg,--ignore,--hidden,--files',
-          key = 'f',
-        },
-        {
-          icon = ' ',
-          desc = 'Apps',
-          group = 'String',
-          action = 'Telescope app',
-          key = 'a',
-        },
-      },
-    },
-  })
+	local db = require("dashboard")
+	db.setup({
+		theme = "hyper",
+		config = {
+			week_header = {
+				enable = true,
+			},
+			project = {
+				enable = true,
+			},
+			disable_move = true,
+			shortcut = {
+				{
+					desc = "Update",
+					icon = " ",
+					group = "Include",
+					action = "Lazy update",
+					key = "u",
+				},
+				{
+					icon = " ",
+					desc = "Files",
+					group = "Function",
+					action = "Telescope find_files find_command=rg,--ignore,--hidden,--files",
+					key = "f",
+				},
+				{
+					icon = " ",
+					desc = "Apps",
+					group = "String",
+					action = "Telescope app",
+					key = "a",
+				},
+			},
+		},
+	})
 end
 
 function config.nvim_tree()
-  require('nvim-tree').setup({
-    -- 完全禁止内置netrw
-    disable_netrw = false,
-    hijack_cursor = true,
-    hijack_netrw = true,
-    -- 隐藏 .文件 和 node_modules 文件夹
-    filters = {
-      dotfiles = true,
-      -- 隐藏 node_modules 文件夹
-      -- custom = { 'node_modules' },
-    },
-    view = {
-      -- 宽度
-      width = 30,
-      -- 左/右
-      side = 'left',
-      -- 隐藏根目录
-      hide_root_folder = false,
-      -- 不显示行数
-      -- number = false,
-      relativenumber = true,
-      -- 显示图标
-      signcolumn = "yes",
-    },
-  })
-  -- 自动关闭
-  vim.cmd([[
+	require("nvim-tree").setup({
+		-- 完全禁止内置netrw
+		disable_netrw = false,
+		hijack_cursor = true,
+		hijack_netrw = true,
+		-- 隐藏 .文件 和 node_modules 文件夹
+		filters = {
+			dotfiles = true,
+			-- 隐藏 node_modules 文件夹
+			-- custom = { 'node_modules' },
+		},
+		view = {
+			-- 宽度
+			width = 30,
+			-- 左/右
+			side = "left",
+			-- 隐藏根目录
+			hide_root_folder = false,
+			-- 不显示行数
+			-- number = false,
+			relativenumber = true,
+			-- 显示图标
+			signcolumn = "yes",
+		},
+	})
+	-- 自动关闭
+	vim.cmd([[
     autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
   ]])
 end
 
 function config.gitsigns()
-  require('gitsigns').setup({
-    signs = {
-      add = { hl = 'GitGutterAdd', text = '▍' },
-      change = { hl = 'GitGutterChange', text = '▍' },
-      delete = { hl = 'GitGutterDelete', text = '▍' },
-      topdelete = { hl = 'GitGutterDeleteChange', text = '▔' },
-      changedelete = { hl = 'GitGutterChange', text = '▍' },
-      untracked = { hl = 'GitGutterAdd', text = '▍' },
-    },
-    keymaps = {
-      -- Default keymap options
-      noremap = true,
-      buffer = true,
+	require("gitsigns").setup({
+		signs = {
+			add = { hl = "GitGutterAdd", text = "▍" },
+			change = { hl = "GitGutterChange", text = "▍" },
+			delete = { hl = "GitGutterDelete", text = "▍" },
+			topdelete = { hl = "GitGutterDeleteChange", text = "▔" },
+			changedelete = { hl = "GitGutterChange", text = "▍" },
+			untracked = { hl = "GitGutterAdd", text = "▍" },
+		},
+		keymaps = {
+			-- Default keymap options
+			noremap = true,
+			buffer = true,
 
-      ['n ]g'] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
-      ['n [g'] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
+			["n ]g"] = { expr = true, "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'" },
+			["n [g"] = { expr = true, "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
 
-      ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-      ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-      ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-      ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-      ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+			["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+			["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+			["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+			["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+			["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>',
 
-      -- Text objects
-      ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-      ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-    },
-  })
+			-- Text objects
+			["o ih"] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+			["x ih"] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+		},
+	})
 end
 
 function config.indent_blankline()
-  require('indent_blankline').setup({
-    char = '│',
-    use_treesitter_scope = true,
-    show_first_indent_level = true,
-    show_current_context = false,
-    show_current_context_start = false,
-    show_current_context_start_on_current_line = false,
-    filetype_exclude = {
-      'dashboard',
-      'log',
-      'fugitive',
-      'gitcommit',
-      'packer',
-      'markdown',
-      'json',
-      'txt',
-      'vista',
-      'help',
-      'todoist',
-      'git',
-      'TelescopePrompt',
-      'undotree',
-    },
-    buftype_exclude = { 'terminal', 'nofile', 'prompt' },
-    context_patterns = {
-      'class',
-      'function',
-      'method',
-      'block',
-      'list_literal',
-      'selector',
-      '^if',
-      '^table',
-      'if_statement',
-      'while',
-      'for',
-    },
-  })
+	require("indent_blankline").setup({
+		char = "│",
+		use_treesitter_scope = true,
+		show_first_indent_level = true,
+		show_current_context = false,
+		show_current_context_start = false,
+		show_current_context_start_on_current_line = false,
+		filetype_exclude = {
+			"dashboard",
+			"log",
+			"fugitive",
+			"gitcommit",
+			"packer",
+			"markdown",
+			"json",
+			"txt",
+			"vista",
+			"help",
+			"todoist",
+			"git",
+			"TelescopePrompt",
+			"undotree",
+		},
+		buftype_exclude = { "terminal", "nofile", "prompt" },
+		context_patterns = {
+			"class",
+			"function",
+			"method",
+			"block",
+			"list_literal",
+			"selector",
+			"^if",
+			"^table",
+			"if_statement",
+			"while",
+			"for",
+		},
+	})
 end
 
 return config
--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -1,33 +1,33 @@
-local conf = require('modules.ui.config')
+local conf = require("modules.ui.config")
 
 packadd({
-  'glepnir/dashboard-nvim',
-  event = 'VimEnter',
-  config = conf.dashboard,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"glepnir/dashboard-nvim",
+	event = "VimEnter",
+	config = conf.dashboard,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 })
 
 packadd({
-  'nvimdev/whiskyline.nvim',
-  config = conf.whisky,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvimdev/whiskyline.nvim",
+	config = conf.whisky,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 })
 
 packadd({
-  'lewis6991/gitsigns.nvim',
-  event = { 'BufRead', 'BufNewFile' },
-  config = conf.gitsigns,
+	"lewis6991/gitsigns.nvim",
+	event = { "BufRead", "BufNewFile" },
+	config = conf.gitsigns,
 })
 
 packadd({
-  'nvim-tree/nvim-tree.lua',
-  cmd = 'NvimTreeToggle',
-  config = conf.nvim_tree,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-tree/nvim-tree.lua",
+	cmd = "NvimTreeToggle",
+	config = conf.nvim_tree,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 })
 
 packadd({
-  'lukas-reineke/indent-blankline.nvim',
-  event = 'BufRead',
-  config = conf.indent_blankline,
+	"lukas-reineke/indent-blankline.nvim",
+	event = "BufRead",
+	config = conf.indent_blankline,
 })
